1a79acd69f00a04f75f140347af4478b5b74dd76
Abdullah <geekyhacks22@gmail.com>
2023-10-21 20:59:28 +0300

    update the order and number of arguments when creating a Teacher object

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(specialization, age, name: 'Unknown')
-    super(age: age, name: name)
+  def initialize(specialization, age, name)
+    super(age, name)
     @specialization = specialization
   end
 
