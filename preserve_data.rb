8db9daf37595190020591bb3ad7a4063830674f3
ishimwepati <wazacode@gmail.com>
2023-10-25 23:42:14 +0300

    Preserved file update

--- preserve_data.rb
+++ preserve_data.rb
@@ -1,13 +1,16 @@
 require 'json'
 
+# mixin
 module PreserveData
+
   @@serializer = JSON
 
   def serialize
     obj = {}
-    instance_variables.each do |var|
+    instance_variables.map do |var|
       obj[var] = instance_variable_get(var)
     end
+
     @@serializer.dump obj
   end
 
@@ -16,17 +19,5 @@ module PreserveData
     obj.keys.each do |key|
       instance_variable_set(key, obj[key])
     end
-  end
-
-  def save_to_json_file(filename)
-    data = self.serialize
-    File.open(filename, 'w') { |file| file.write(data) }
-  end
-
-  def load_from_json_file(filename)
-    return unless File.exist?(filename)
-
-    content = File.read(filename)
-    self.unserialize(content)
   end
 end
