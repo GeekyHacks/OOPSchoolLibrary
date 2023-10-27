90248aa916c59e4fe88b17794e8d8476053dfd87
Abdullah <geekyhacks22@gmail.com>
2023-10-28 02:03:34 +0300

    Edit the rental test file

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,21 +1,20 @@
 require './rental'
 
 describe Rental do
-  let(:book) { double('Book') }
-  let(:person) { double('Person') }
-
-  before do
-    allow(book).to receive(:rentals).and_return([]) # Configure book double to respond to rentals method
-    allow(person).to receive(:rentals).and_return([]) # Configure person double to respond to rentals method
-  end
+  let(:book) { double('Book', rentals: []) }
+  let(:person) { double('Person', rentals: []) }
 
   subject(:rental) { described_class.new('2023-10-26', book, person) }
 
   it { is_expected.to have_attributes(date: '2023-10-26', book: book, person: person) }
 
   it 'associates the rental with the book and person' do
-    expect(book).to receive(:rentals=).with([rental])
-    expect(person).to receive(:rentals=).with([rental])
+    expect(book.rentals).to be_empty
+    expect(person.rentals).to be_empty
+
     rental
+
+    expect(book.rentals).to contain_exactly(rental)
+    expect(person.rentals).to contain_exactly(rental)
   end
 end
