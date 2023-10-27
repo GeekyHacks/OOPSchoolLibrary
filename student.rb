fb0ca93613de61afb8a122a9ff2888558b2a48b8
ishimwepati <wazacode@gmail.com>
2023-10-27 03:58:35 +0300

    Most file updated in order for the storage to work

--- student.rb
+++ student.rb
@@ -1,19 +1,21 @@
 require_relative 'person'
 
 class Student < Person
-  attr_reader :classroom
+  attr_accessor :classroom, :age, :name, :id
+  attr_reader :label, :parent_permission
 
-  def initialize(age, name, parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  def initialize(age, name = 'Unknown', parent_permission: true, id: nil)
+    super(age, name, parent_permission: parent_permission, id: id)
+    @classroom = nil
+    @label = 'Student'
+  end
+
+  def assign_classroom(classroom)
     @classroom = classroom
+    classroom.student_group.push(self) unless classroom.student_group.include?(self)
   end
 
   def play_hooky
-    '¯\\_(ツ)_/¯'
-  end
-
-  def classroom=(classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
+    '¯(ツ)/¯'
   end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,9 +1,13 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(specialization, age, name)
-    super(age, name)
+  attr_reader :label
+  attr_accessor :age, :name, :specialization, :id
+
+  def initialize(age, specialization, name = 'Unknown', id: nil)
+    super(age, name, id: id)
     @specialization = specialization
+    @label = 'Teacher'
   end
 
   def can_use_services?
