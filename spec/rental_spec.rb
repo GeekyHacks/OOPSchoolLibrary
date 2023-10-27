cff89e26c348380d9362eea95e61172eb3ed94c3
Ishimwe JOSEPH WazaCode <wazacode@gmail.com>
2023-10-28 02:06:00 +0300

    Update rental_spec.rb

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -9,12 +9,11 @@ describe Rental do
   it { is_expected.to have_attributes(date: '2023-10-26', book: book, person: person) }
 
   it 'associates the rental with the book and person' do
-    expect(book.rentals).to be_empty
-    expect(person.rentals).to be_empty
+    allow(book).to receive(:rentals).and_return([])  # Clear any previous configurations
+    allow(person).to receive(:rentals).and_return([])  # Clear any previous configurations
 
+    expect(book).to receive(:rentals=).with([rental])
+    expect(person).to receive(:rentals=).with([rental])
     rental
-
-    expect(book.rentals).to contain_exactly(rental)
-    expect(person.rentals).to contain_exactly(rental)
   end
 end
