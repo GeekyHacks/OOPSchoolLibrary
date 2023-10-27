e1d283322c44b7cb75f40957702810129d3cf2e8
Abdullah <geekyhacks22@gmail.com>
2023-10-28 02:00:09 +0300

    Edit the book and student test cases

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,6 +1,35 @@
 require './book'
+require './rental'
 
-describe Book.new 'Microverse', 'Autum Johe' do 
-    it { is_expected.to have_attributes(title: 'Microverse') } 
-    it { is_expected.to have_attributes(author: 'Autum Johe') }
- end
+describe Book do
+  before(:each) do
+    @book = Book.new('title', 'author')
+    @rentals = []
+  end
+
+  describe '#new' do
+    it 'returns a new book object' do
+      expect(@book).to be_an_instance_of(Book)
+    end
+  end
+
+  describe '#add_rental' do
+    after(:each) do
+      @rentals = [@rental]
+    end
+
+    it 'returns a new rental object' do
+      person = double('Person', rentals: [])
+      @rental = Rental.new('2020-10-10', @book, person)
+
+      expect(@rental).to be_an_instance_of(Rental)
+    end
+
+    it 'adds rental to person rentals list when add_rental method is called' do
+      person = double('Person', rentals: [])
+      @rental = Rental.new('2020-10-10', @book, person)
+
+      expect(person.rentals).to include(@rental)
+    end
+  end
+end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,7 +1,7 @@
 require './student'
 
 describe Student.new 'IT', 24, 'Abdu' do
-
+    it { is_expected.to be_a(Person) } 
     it{is_expected.to have_attributes(classroom: 'IT') } 
     it{is_expected.to have_attributes(age: 24) } 
     it{is_expected.to have_attributes(name: 'Abdu') } 
