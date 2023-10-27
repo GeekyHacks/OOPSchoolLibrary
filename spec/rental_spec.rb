a121af96818e2c4bb8245f41a58f0389427c8547
ishimwepati <wazacode@gmail.com>
2023-10-27 22:33:46 +0300

    Renta test added

---
+++ spec/rental_spec.rb
@@ -0,0 +1,15 @@
+require './rental'
+
+describe Rental do
+  let(:book) { double('Book') }
+  let(:person) { double('Person') }
+  subject(:rental) { described_class.new('2023-10-26', book, person) }
+
+  it { is_expected.to have_attributes(date: '2023-10-26', book: book, person: person) }
+
+  it 'associates the rental with the book and person' do
+    expect(book).to receive(:rentals=).with([rental])
+    expect(person).to receive(:rentals=).with([rental])
+    rental
+  end
+end
